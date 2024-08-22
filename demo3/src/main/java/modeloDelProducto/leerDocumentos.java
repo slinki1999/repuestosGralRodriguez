0d5b9b6df859ed3329c7a5dcac8274611a7204c0
slinki1999 <87882642+slinki1999@users.noreply.github.com>
2024-08-22 18:30:30 -0300

    Add files via upload

--- demo3/src/main/java/modeloDelProducto/leerDocumentos.java
+++ demo3/src/main/java/modeloDelProducto/leerDocumentos.java
@@ -2,14 +2,18 @@ package modeloDelProducto;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class leerDocumentos {
     
     public static void main(String[] args) {
         
-        String archivo="C:\\Users\\criss\\Desktop\\Nueva carpeta\\demo3\\src\\main\\java\\modeloDelProducto\\retenesSueltos.txt";
-        try (BufferedReader buffer=new BufferedReader(new FileReader(archivo));){
+        //Leer documento
+        String rutaArchivo="C:\\Users\\criss\\Desktop\\Nueva carpeta\\demo3\\src\\main\\java\\modeloDelProducto\\retenesSueltos.txt";
+        try (BufferedReader buffer=new BufferedReader(new FileReader(rutaArchivo))){
             String cadena;
             int cont=0;
             while ((cadena=buffer.readLine())!=null) {
@@ -21,7 +25,21 @@ public class leerDocumentos {
             System.out.println("Error de lectura!"+e.getMessage());
         }
         
-
+        //Escribir en un documento
+        try (FileWriter fileWriter = new FileWriter(rutaArchivo);
+             PrintWriter printWriter = new PrintWriter(fileWriter)) {
+            
+            // Escribir el contenido en el archivo
+            Scanner input= new Scanner(System.in);
+            printWriter.println(input.next());
+            
+            // Confirmación de escritura
+            System.out.println("El archivo se ha escrito correctamente.");
+            
+        } catch (IOException e) {
+            // Manejo de posibles excepciones
+            System.out.println("Ocurrió un error al escribir el archivo: " + e.getMessage());
+        }
         
     }
 }
